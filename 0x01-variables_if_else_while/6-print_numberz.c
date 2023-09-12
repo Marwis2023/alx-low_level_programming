d3d164aa8525cae0c23bee574e40df8632db0230
Marwis2023 <grahamwisdom90@gmail.com>
2023-09-11 21:30:25 -0700

    Printt numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print numbers 0 to 9
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <= 9; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+
+	return (0);
+}
